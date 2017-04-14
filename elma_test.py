b168dd2cff94f3f1e1b0745460d60b27075c427f
user-web <kontent3@sovetnikprof.ru>
2017-04-14 10:22:37 +0300

    copy() in get_dict()
    Styling source, some comments

--- elma_test.py
+++ elma_test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 import requests as req
-import jsonpickle as json
+import jsonpickle
+from copy import copy
 from datetime import datetime, timedelta
 
 app_token = '132D5D0E49B0D30528CB4FEF5FA1FED73FC0DB202C0C1102EE0778B13446A2D89F213BB8BB09BEF22DD09635CBEAF6805E1CEEBD3BA10D844FE635AECE90CA8B'
@@ -9,7 +10,11 @@ username = 'ia.chechetkin'
 password = '123'
 host = 'http://88.87.81.251:1313/API/REST/'
 
-urls = {'auth': 'Authorization/LoginWith',
+# ----- some definitions -----
+
+# dictionary of urls to shorten requests
+urls = {'auth_login': 'Authorization/LoginWith',
+        'auth_time': 'Authorization/ServerTime',
 
         'task_create': 'Tasks/Create',
         'task_close': 'Tasks/Close',
@@ -26,35 +31,25 @@ urls = {'auth': 'Authorization/LoginWith',
         'file_download': 'Files/Download',
         'file_size': 'Files/FileSize',
 
-        'process_start': 'Workflow/StartProcessAsync'
+        'process_start': 'Workflow/StartProcessAsync',
+        'process_status': 'Workflow/ExecuteUserTaskStatus'
        }
 
+# in-code url shortener
 def url(key, value=None):
     return host + urls[key] + ('/{}'.format(value) if value else '')
 
+# making session
 ses = req.Session()
 
-# ----- auth -----
-
+# definitions for auth
 headers = {'ApplicationToken': app_token,
            'Content-Type': 'application/json; charset=utf-8'}
+auth_token = None
+session_token = None
+current_user = None
 
-r = ses.post(url('auth'),
-             params={'username': username},
-             headers=headers,
-             data=password)
-r = r.json()
-
-auth_token = r['AuthToken']
-session_token = r['SessionToken']
-current_user = r['CurrentUserId']
-
-headers = {'ApplicationToken': app_token,
-           'AuthToken': auth_token,
-           'Content-Type': 'application/json; charset=utf-8'}
-
-# ----- some definitions -----
-
+# Web-API Data Item
 class Item:
     def __init__(self, name, value=None, data=None, dataarray=None):
         self.Name = name
@@ -90,6 +85,7 @@ class Item:
     def __str__(self):
         return str(self.__dict__)
 
+# Web-API Item List
 class Data:
     def __init__(self, *args):
         self.Items = []
@@ -103,6 +99,7 @@ class Data:
     def __str__(self):
         return str(self.__dict__)
 
+# Web-API List of Items Lists
 class DataArray(list):
     def add_datas(self, *args):
         for data in args:
@@ -112,19 +109,23 @@ class DataArray(list):
         for data in args:
             self.append(data)
 
+# returns list of dictionaries built on every 'Items' element
+# used for easy getting parameters: instead of use 'Name' filter on ['Items'][#] use get_dict[#][Name] to get data from parameter
 def get_dict(json_):
     results = []
     if type(json_) != list: json_ = [json_]
     for el in json_:
         results.append({})
         for i in el['Items']:
-            results[-1][i['Name']] = i
+            results[-1][i['Name']] = copy(i)
             results[-1][i['Name']].pop('Name', None)
     return results
 
+# alias for jsonpickle function
 def get_json(d):
-    return json.encode(d, unpicklable=False)
+    return jsonpickle.encode(d, unpicklable=False)
 
+# pretty alias for datetime().strtime
 def date(day=None, month=None, year=None, hour=None, minute=None, now=False):
     if now:
         return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -143,6 +144,24 @@ def date(day=None, month=None, year=None, hour=None, minute=None, now=False):
 
 # ~~ Working Functions ~~ #
 
+# ----- auth -----
+
+def login(username, password):
+    r = ses.post(url('auth_login'),
+                 params={'username': username},
+                 headers=headers,
+                 data=password)
+    print('<< Request: ' + r.url)
+    print('>> Response: ' + r.text)
+    return r
+
+def server_time():
+    r = ses.get(url('auth_time'),
+                headers=headers)
+    print('<< Request: ' + r.url)
+    print('>> Server time: ' + r.text)
+    return r
+
 # ----- files -----
 
 def file_upload(file_):
@@ -156,6 +175,8 @@ def file_upload(file_):
                     data=f)
 
     id = r.text[1:-1]
+
+    print('<< Request: ' + r.url)
     print('>> File uploaded: ' + id)
     return id
 
@@ -163,21 +184,26 @@ def file_size(id):
     r = ses.get(url('file_size'),
                 params={'uid': str(id)},
                 headers=headers)
+    print('<< Request: ' + r.url)
     print('>> File size: ' + r.text)
     return r
 
 def file_download(id):
     size = file_size(id)
+
     r = ses.get(url('file_download'),
                  params={'uid': str(id)},
                  headers=headers,
                  stream=True)
+    print('<< Request: ' + r.url)
+
     local = r.headers.get('Content-Disposition')
     local = list(filter(lambda x: x.find('filename') > -1, local.split(';')))[0].split('=')[-1]
     with open(local, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
+
     print('>> File downloaded to ' + local)
     return local
 
@@ -207,6 +233,7 @@ def query_entity(typeid, query=None, filter_=None, limit=None, offset=None, sort
         params['filterProviderData'] = str(filterdata)
     if filter_:
         params['filter'] = str(filter_)
+
     r = ses.get(url('entity_query'),
                 params=params,
                 headers=headers)
@@ -414,3 +441,30 @@ def start_process(name, context, process_token=None, process_header=None):
                  data=jsn)
     print('>> Response: ' + r.text)
     return r
+
+def process_status(token):
+    d = Data()
+
+    etok = Item('ExecutionToken', str(token))
+    d.add_items(etok)
+
+    jsn = get_json(d)
+
+    print('<< Request: ' + jsn)
+    r = ses.post(url('process_status'),
+                 headers=headers,
+                 data=jsn)
+    print('>> Response: ' + r.text)
+    return r
+
+if __name__ == '__main__':
+    l = login(username, password)
+    l = l.json()
+
+    try:
+        auth_token = l['AuthToken']
+        session_token = l['SessionToken']
+        current_user = l['CurrentUserId']
+        headers['AuthToken'] = auth_token
+    except KeyError:
+        raise Exception('Failed to log in!')
