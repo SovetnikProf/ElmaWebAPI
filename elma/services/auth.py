560672fc764dcae1377c4ca0e8403301e60c2903
Чечёткин Илья <sovetnikprof.web03@gmail.com>
2022-04-05 17:55:13 +0300

    feat(auth): добавлен метод `AuthService.ServerTime`

--- elma/services/auth.py
+++ elma/services/auth.py
@@ -6,6 +6,7 @@ import requests
 
 
 LOGIN_WITH = "/API/REST/Authorization/LoginWith"
+SERVER_TIME = "/API/REST/Authorization/ServerTime"
 
 
 class AuthService(Service):
@@ -30,5 +31,16 @@ class AuthService(Service):
         except requests.RequestException:
             raise ConnectionError(response.text)
 
-    def ServerTime(self, session):
-        pass
+    @needs_auth
+    @get(url=SERVER_TIME)
+    def ServerTime(self, result: requests.Response) -> datetime:
+        # result.json() == "/Date(1234567890123+0300)/"
+        time: str = result.json().strip("/").replace("Date(", "").replace(")", "")
+        char = "+" if "+" in time else "-"
+        ts, tz = time.split("+")
+
+        ts = int(ts) / 1000.0  # python timestamp is 1234567890.123
+        tz = datetime.strptime(f"{char}{tz}", "%z").tzinfo
+
+        dt = datetime.fromtimestamp(ts, tz=tz)
+        return dt
