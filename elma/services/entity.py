ba96bf09004c7483060893f0670abe3308f00179
Чечёткин Илья <sovetnikprof.web03@gmail.com>
2022-08-23 17:58:22 +0300

    fix: возвращаемый результат у методов `EntityService`
    Closes #1

--- elma/services/entity.py
+++ elma/services/entity.py
@@ -21,19 +21,22 @@ class EntityService(base.Service):
 
     @decorators.needs_auth
     @decorators.get(url=LOAD)
-    def Load(self, result: "Response", *args, **kwargs):
+    def Load(self, result: "Response", *args, **kwargs) -> dict:
         """
         Получить сущность по типу и идентификатору.
 
         Параметры (для передачи через `params`):
             - type (str): идентификатор типа сущности
             - id (int): идентификатор объекта
+
+        Returns:
+            dict: нормализованный словарь с данными объекта
         """
         return Parser.normalize(result.json())
 
     @decorators.needs_auth
     @decorators.get(url=COUNT)
-    def Count(self, result: "Response", *args, **kwargs):
+    def Count(self, result: "Response", *args, **kwargs) -> int:
         """
         Получить количество сущностей данного типа.
 
@@ -46,12 +49,15 @@ class EntityService(base.Service):
                           Наименование свойства возможно задавать с точкой (.) для получения доступа к подсвойству:
                           Property1.Property2:Значение1. Для указания в значении свойства символа : (двоеточие),
                           \\ (обратный слэш) или , (запятая), его нужно экранировать через \\ (обратный слэш).
+
+        Returns:
+            int: количество объектов
         """
-        return Parser.normalize(result.json())
+        return int(result.text)
 
     @decorators.needs_auth
     @decorators.get(url=QUERY)
-    def Query(self, result: "Response", *args, **kwargs):
+    def Query(self, result: "Response", *args, **kwargs) -> list:
         """
         Получить все сущности данного типа, отфильтрованные по запросу.
 
@@ -67,30 +73,36 @@ class EntityService(base.Service):
                           Наименование свойства возможно задавать с точкой (.) для получения доступа к подсвойству:
                           Property1.Property2:Значение1. Для указания в значении свойства символа : (двоеточие),
                           \\ (обратный слэш) или , (запятая), его нужно экранировать через \\ (обратный слэш)
+
+        Returns:
+            list: список нормализованных словарей с данными объектов
         """
         return Parser.normalize(result.json())
 
     @decorators.needs_auth
     @decorators.post(url=INSERT)
-    def _insert(self, result: "Response", *args, **kwargs):
-        return Parser.normalize(result.json())
+    def _insert(self, result: "Response", *args, **kwargs) -> int:
+        return int(result.text.replace('"', ""))
 
-    def Insert(self, entityData: dict, typeuid: str, *args, **kwargs):
+    def Insert(self, entityData: dict, typeuid: str, *args, **kwargs) -> int:
         """
         Сохранить новый объект в системе.
 
         Args:
             typeuid: идентификатор типа сущности
             entityData: данные объекта в формате WebData
+
+        Returns:
+            int: id нового объекта
         """
         return self._insert(data=entityData, uri=INSERT.format(typeuid=typeuid), *args, **kwargs)
 
     @decorators.needs_auth
     @decorators.post(url=UPDATE)
-    def _update(self, result: "Response", *args, **kwargs):
-        return Parser.normalize(result.json())
+    def _update(self, result: "Response", *args, **kwargs) -> int:
+        return int(result.text.replace('"', ""))
 
-    def Update(self, entityData: dict, typeuid: str, entityid: int, *args, **kwargs):
+    def Update(self, entityData: dict, typeuid: str, entityid: int, *args, **kwargs) -> int:
         """
         Обновить существующий объект в системе.
 
@@ -98,5 +110,8 @@ class EntityService(base.Service):
             typeuid: идентификатор типа сущности
             entityid: идентификатор объекта
             entityData: данные объекта в формате WebData
+
+        Returns:
+            int: id измененного объекта, должен совпадать с переданным entityid
         """
         return self._update(data=entityData, uri=UPDATE.format(typeuid=typeuid, entityid=entityid), *args, **kwargs)
